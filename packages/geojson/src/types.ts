eb5ace3d396d1fad9283a9eba580c201bff102fe
jessekrubin <jessekrubin@gmail.com>
2023-07-24 10:44:17 -0700

    remove types from geojson pkg

--- packages/geojson/src/types.ts
+++ packages/geojson/src/types.ts
@@ -1,7 +1,1 @@
-import type * as geojsonTypes from "./geojson-types.js";
-export * from "./geojson-types.js";
-export * from "./tilejson-types.js";
-
-
-// keys of geojsonTypes
-export type Types = keyof typeof geojsonTypes;
+export * from '@jsse/geotypes';
