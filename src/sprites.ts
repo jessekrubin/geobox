2fc8ec5bc64fafb42c1056d5cd343b8b3a5ba78f
jessekrubin <jessekrubin@gmail.com>
2024-02-23 10:49:58 -0800

    sprite-sheets

---
+++ src/sprites.ts
@@ -0,0 +1,59 @@
+// pub struct SpriteDescription {
+//     pub height: u32,
+//     pub pixel_ratio: u8,
+//     pub width: u32,
+//     pub x: u32,
+//     pub y: u32,
+//     pub content: Option<Rect>,
+//     pub stretch_x: Option<Vec<Rect>>,
+//     pub stretch_y: Option<Vec<Rect>>,
+//     pub sdf: bool,
+// }
+import type { SchemaOptions } from "./typebox.js";
+import { Type } from "./typebox.js";
+import { Latitude, Longitude } from "./coord.js";
+
+function SpriteDescriptionStretch() {
+  return Type.Array(Type.Tuple([Type.Integer(), Type.Integer()]), {
+    description:
+      "stretchX/stretchY: An array of two-element arrays, consisting of two numbers that represent the from position and the to position of areas that can be stretched.",
+  });
+}
+
+export function SpriteDescription(options?: SchemaOptions) {
+  return Type.Object(
+    {
+      height: Type.Integer({
+        description: "Height of the icon in pixels.",
+      }),
+      pixel_ratio: Type.Integer({
+        description: "Pixel ratio of the sprite. Defaults to 1.",
+      }),
+      width: Type.Integer({
+        description: "Width of the icon in pixels.",
+      }),
+      x: Type.Integer({
+        description: "X coordinate of the icon image in the sprite.",
+      }),
+      y: Type.Integer({
+        description: "Y coordinate of the icon image in the sprite.",
+      }),
+      content: Type.Optional(
+        Type.Tuple(
+          [Type.Integer(), Type.Integer(), Type.Integer(), Type.Integer()],
+          {
+            description:
+              "Content rect: content: An array of four numbers, with the first two specifying the left, top corner, and the last two specifying the right, bottom corner. If present, and if the icon uses icon-text-fit, the symbol's text will be fit inside the content box.",
+          },
+        ),
+      ),
+      stretch_x: Type.Optional(SpriteDescriptionStretch()),
+      stretch_y: Type.Optional(SpriteDescriptionStretch()),
+      sdf: Type.Boolean({
+        description:
+          "sdf: Boolean. If true then the image is handled as a signed-distance field (SDF) and its color can be set at runtime using the icon-color and icon-halo-color properties. Defaults to false.",
+      }),
+    },
+    options,
+  );
+}
