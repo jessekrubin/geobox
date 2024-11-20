6e36a5db8ed9d6dbf8888c134966325d51214ac5
jessekrubin <jessekrubin@gmail.com>
2024-11-20 06:08:26 -0800

    eslint fix

--- src/geo-type/tms/index.ts
+++ src/geo-type/tms/index.ts
@@ -1,4 +1,4 @@
-export { TileMatrix } from "./tile-matrix.js";
 export { TileMatrixLimits } from "./tile-matrix-limits.js";
 export { TileMatrixSet } from "./tile-matrix-set.js";
+export { TileMatrix } from "./tile-matrix.js";
 export { TileSet } from "./tile-set.js";
